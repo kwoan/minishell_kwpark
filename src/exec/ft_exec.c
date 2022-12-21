b63f809fea909f0cc10dc53aaf8b684af9ef6077
Taehyun Kim <taehyunk@c8r5s14.42seoul.kr>
2022-12-21 17:57:19 +0900

    export mod

--- src/exec/ft_exec.c
+++ src/exec/ft_exec.c
@@ -6,7 +6,7 @@
 /*   By: taehyunk <taehyunk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 13:22:31 by taehyunk          #+#    #+#             */
-/*   Updated: 2022/12/21 16:59:50 by taehyunk         ###   ########seoul.kr  */
+/*   Updated: 2022/12/21 17:56:39 by taehyunk         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,9 +61,23 @@ int	ft_exec(t_list *env_lst, char **argv)
 	{
 		envp = lst_to_arr(env_lst);
 		path = find_path(envp);
-		check_cmd(&argv[0], path);
-		if (execve(argv[0], argv, envp) == -1)
-			printf("minishell: %s : command not found\n", argv[0]);
+		if (ft_strchr(argv[0], '/') && access(argv[0], F_OK) == -1)
+			print_error_msg2(argv[0], "", "No such file or directory\n");
+		else
+		{
+			check_cmd(&argv[0], path);
+			if (execve(argv[0], argv, envp) == -1)
+			{
+				if (access(argv[0], F_OK) == 0 && access(argv[0], X_OK) == -1)
+					print_error_msg2(argv[0], "", "Permission denied\n");
+				else if (access(argv[0], F_OK) == 0)
+					print_error_msg2(argv[0], "", "is a directory\n");
+				else if (access(argv[0], F_OK) == 0)
+					print_error_msg2(argv[0], "", "Not a directory\n");
+				else
+					print_error_msg2(argv[0], "", "command not found\n");
+			}
+		}
 		free_str_arr(envp);
 		free_str_arr(path);
 		exit(127);
